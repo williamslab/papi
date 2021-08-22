b8f0a75c7525b99c961b8da9d0487a200472ff5c
siddharth51292 <siddharth.avadhanam@gmail.com>
2021-08-22 16:08:10 +0530

    Added error model options

--- src/inference.py
+++ src/inference.py
@@ -9,6 +9,7 @@ import json
 import sys
 import scipy
 from scipy import optimize
+
 # define a theano Op for our likelihood function
 
 class LogLike(tt.Op):
@@ -57,40 +58,177 @@ def filter_tracts(llt):
     for lt in llt:
         lt_proc_zeros = [x for x in lt if not x[1]==0]
         lt_proc_zeros_het =[('10',x[1]) if x[0]=='01' else x for x in lt_proc_zeros]
-        assert all([x[1]>0 for x in lt_proc_zeros_het]),"negative lengths in tract input"
-
         llt_proc.append(lt_proc_zeros_het)
 
     
     return(llt_proc)
 
 def lik_func(params,D):
-    
+ #{{{    
     D_dicts = md.tups2dicts(D)
     D_flat = [{x['type']:x['length']} for y in D_dicts for x in y]
     counter = collections.Counter() 
+
     for D_i in D_flat:  
         counter.update(D_i)     
     D_flat = dict(counter)
+
+    print("Running full model")
     l = md.computeLoglikelihood_binomial(D_flat,params[:2]) + md.computeLoglikelihood_cnsPM(D_dicts,params)
     #l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+#}}}
+    return(l)
+
+def lik_func_bin(params,D):
+#{{{ 
+    D_dicts = md.tups2dicts(D)
+    D_flat = [{x['type']:x['length']} for y in D_dicts for x in y]
+    counter = collections.Counter() 
+
+    for D_i in D_flat:  
+        counter.update(D_i)     
+    D_flat = dict(counter)
+    
+    print("Running binomial model only")
+    l = md.computeLoglikelihood_binomial(D_flat,params[:2])
+    #l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+ #}}}
+    return(l)
+
+def lik_func_mrkv(params,D):
+    #{{{ 
+    D_dicts = md.tups2dicts(D)
+    D_flat = [{x['type']:x['length']} for y in D_dicts for x in y]
+    counter = collections.Counter() 
+
+    for D_i in D_flat:  
+        counter.update(D_i)     
+    D_flat = dict(counter)
+
+    print("running mrkv model only")
+    l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+    #l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+#}}}
     return(l)
 
 
-def estimate_MAP(d_tracts):
+def lik_func_err(params,D):
+ #{{{    
+    D_dicts = md.tups2dicts(D)
+    D_flat = [{x['type']:x['length']} for y in D_dicts for x in y]
+    counter = collections.Counter() 
+
+    for D_i in D_flat:  
+        counter.update(D_i)     
+    D_flat = dict(counter)
+
+    print("Running full model - with error ")
+    l = md.computeLoglikelihood_binomial(D_flat,params[:2]) + md.computeLoglikelihood_cnsPM(D_dicts,params,phi=69.314,err=True)
+    #l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+#}}}
+    return(l)
+
+def lik_func_bin_err(params,D):
+#{{{ 
+    D_dicts = md.tups2dicts(D)
+    D_flat = [{x['type']:x['length']} for y in D_dicts for x in y]
+    counter = collections.Counter() 
+
+    for D_i in D_flat:  
+        counter.update(D_i)     
+    D_flat = dict(counter)
+    
+    print("Running binomial model only")
+    l = md.computeLoglikelihood_binomial(D_flat,params[:2])
+    #l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+ #}}}
+    return(l)
+
+def lik_func_mrkv_err(params,D):
+    #{{{ 
+    D_dicts = md.tups2dicts(D)
+    D_flat = [{x['type']:x['length']} for y in D_dicts for x in y]
+    counter = collections.Counter() 
+
+    for D_i in D_flat:  
+        counter.update(D_i)     
+    D_flat = dict(counter)
+
+    print("running mrkv model only")
+    l = md.computeLoglikelihood_cnsPM(D_dicts,params,phi=69.314,err=True)
+    #l = md.computeLoglikelihood_cnsPM(D_dicts,params)
+#}}}
+    return(l)
+
+
+def estimate_MAP(d_tracts,typ='full',err=False):
     '''
     Function that infers map and CIs using the scipy.optimize module 
     '''
+ #{{{    
+
     
-    bnds = ((0.001, 0.999),(0.001,0.999),(0,25), (0, 25))
-    res = scipy.optimize.minimize(
-        fun=lambda params, D: -lik_func(params,d_tracts),
-        x0=np.array([0.7,0.8,4,7]),
-        args=(d_tracts,),
-        method='L-BFGS-B',
-        bounds=bnds
-    )
-    
+    if typ=='bin':
+        bnds = ((0.001, 0.999),(0.001,0.999),(0,25), (0, 25))
+        if err:
+            res = scipy.optimize.minimize(
+                fun=lambda params, D: -lik_func_bin_err(params,d_tracts),
+                x0=np.array([0.7,0.8,4,7]),
+                args=(d_tracts,),
+                method='L-BFGS-B',
+                bounds=bnds
+            )
+        else:
+            res = scipy.optimize.minimize(
+                fun=lambda params, D: -lik_func_bin(params,d_tracts,err),
+                x0=np.array([0.7,0.8,4,7]),
+                args=(d_tracts,),
+                method='L-BFGS-B',
+                bounds=bnds
+            )
+    elif typ=='mrkv':
+        
+        bnds = ((0.001, 0.999),(0.001,0.999),(0,25), (0, 25))
+        if err:
+            res = scipy.optimize.minimize(
+                fun=lambda params, D: -lik_func_mrkv_err(params,d_tracts),
+                x0=np.array([0.7,0.8,4,7]),
+                args=(d_tracts,),
+                method='L-BFGS-B',
+                bounds=bnds
+            )
+        else:
+            res = scipy.optimize.minimize(
+                fun=lambda params, D: -lik_func_mrkv_err(params,d_tracts),
+                x0=np.array([0.7,0.8,4,7]),
+                args=(d_tracts,),
+                method='L-BFGS-B',
+                bounds=bnds
+            )
+
+    elif typ=='full':
+        
+        bnds = ((0.001, 0.999),(0.001,0.999),(0,25), (0, 25))
+        if err:
+            res = scipy.optimize.minimize(
+                fun=lambda params, D: -lik_func_err(params,d_tracts),
+                x0=np.array([0.7,0.8,4,7]),
+                args=(d_tracts,),
+                method='L-BFGS-B',
+                bounds=bnds
+            )
+        else:
+            res = scipy.optimize.minimize(
+                fun=lambda params, D: -lik_func(params,d_tracts),
+                x0=np.array([0.7,0.8,4,7]),
+                args=(d_tracts,),
+                method='L-BFGS-B',
+                bounds=bnds
+            )
+    else:
+        sys.exit('please specify valide typ - bin,mrkv or full')
+
+#}}}
     return(np.round(res.x,2))
 
 
@@ -110,7 +248,8 @@ if __name__ == "__main__":
     parser.add_argument("--tracefile", "-t", nargs='?', const=None)
     parser.add_argument("--outfile", "-o",type=str, required=True)
     parser.add_argument("--mode", "-m", nargs='?', type=str, const='pymc')
-
+    parser.add_argument("--typ", "-typ", nargs='?', type=str, const='full')
+    parser.add_argument("--err", "-err", default=False, action='store_true')
     # read arguments from the command line
     args = parser.parse_args()
 
@@ -118,15 +257,38 @@ if __name__ == "__main__":
     data_all = pd.read_table(args.inputfile)
     data_ind = data_all.iloc[args.ind-1] 
     d_tracts = filter_tracts(ast.literal_eval(data_ind.tracts))
+    
+    print(args.err)
 
+    if args.mode=='scipy-optimize':
+        print("Running inference in scipy-optimize mode")
+        MAPestimate_scipy = estimate_MAP(d_tracts,typ=args.typ,err=args.err)
+        np.savetxt(args.outfile + '.scipy.map' , MAPestimate_scipy, newline=" ")
 
-    if args.mode==None or args.mode == 'pymc':
-
+    elif args.mode=='pymc' or args.mode==None:
         print("Running inference in pymc mode")
         basic_model = pm.Model()
+
         # create our Op
-        logl = LogLike(lik_func, d_tracts)
-        
+        if args.err==True:
+            if args.typ=='bin':
+                logl = LogLike(lik_func_bin_err, d_tracts)
+            elif args.typ=='mrkv':
+                logl = LogLike(lik_func_mrkv_err, d_tracts)
+            elif args.typ =='full':
+                logl = LogLike(lik_func_err, d_tracts)
+            else:
+                sys.exit("Invalid typ option provided - please specify : bin, mrkv or full")
+        else:
+            if args.typ=='bin':
+                logl = LogLike(lik_func_bin, d_tracts)
+            elif args.typ=='mrkv':
+                logl = LogLike(lik_func_mrkv, d_tracts)
+            elif args.typ =='full':
+                logl = LogLike(lik_func, d_tracts)
+            else:
+                sys.exit("Invalid typ option provided - please specify : bin, mrkv or full")
+
         # use PyMC3 to sampler from log-likelihood
         with basic_model:
             # Priors for unknown model parameters
@@ -143,25 +305,20 @@ if __name__ == "__main__":
 
             mean_q = pm.find_MAP(model=basic_model)
             #std_q = ((1/pm.find_hessian(mean_q))**0.5)[0]
+            
 
             if args.tracefile:
-                #full mcmc
-                print("Running Full MCMC")
+            #full mcmc
                 trace = pm.sample(1500)
+                pm.save_trace(trace, args.tracefile+'.trace',overwrite=True) 
                 df = az.summary(trace,credible_interval=0.9)
-                df.to_csv(args.outfile[:-4] + '.summ',sep='\t')
-                pm.save_trace(trace, args.tracefile,overwrite=True) 
-                
-            
-            with open(args.outfile, 'w') as f:
+                df.to_csv(args.outfile,sep='\t')
+
+            with open(args.outfile + '.map', 'w') as f:
                 print(mean_q, file=f)
 
-
-
-    if args.mode=='scipy-optimize':
-        print("Running inference in scipy-optimize mode")
-        MAPestimate_scipy = estimate_MAP(d_tracts)
-        np.savetxt(args.outfile, MAPestimate_scipy, newline=" ")
+    else:
+        sys.exit("Invalid mode option - please provide either 'pymc' or 'scipy-optimize'")
 
 #}}}
 
--- src/models.py
+++ src/models.py
@@ -144,8 +144,30 @@ def err_func_v0(d,k):
     x = d['length']/100
     return(np.exp(-k*x))
 
+def get_emissions(d,d_prev,k,loge):
+ #{{{   
+    nonErrorLogProb=np.log(1-np.exp(loge))
+    
+    if k == 1 or k == 2:
+        if d_prev['type'] == '00':
+            emiss = [loge,nonErrorLogProb,-np.inf,-np.inf]
+        elif d_prev['type'] == '11':
+            emiss = [-np.inf,nonErrorLogProb,-np.inf,loge]
+        else:
+            emiss = [loge-np.log(2),nonErrorLogProb,-np.inf,loge-np.log(2)] #error probs are dsitributed across '00' and '11' if prev state is het
 
-def computeLoglikelihood_cnsPM(D, pars, phi=69.314):
+    elif k==0:
+        if d_prev['type'] == '00':
+            emiss = [nonErrorLogProb,loge-np.log(2),-np.inf,-np.inf]
+        if d_prev['type'] == '0':
+            emiss = [nonErrorLogProb,loge-np.log(2),-np.inf,-np.inf]
+
+    else:
+        sys.exit('invalid key')
+#}}}
+    return()
+
+def computeLoglikelihood_cnsPM(D, pars, phi=69.314,err=False):
 
     '''
     Takes in dict of parameter values ( e.g : [0.3,0.5,6,5]}) and the data D for ALL chromsomes ({'type':'10', 'length':1018} ... ). Additionally takes in an error function and parameters phi for the error function ( see err_func). Also takes in bg which is a dictionary containing median number of tracts and background tract summary statistics from individual for use with error function. Computes loglikelihood of data under our pooled markov model with censoring ( cns). 
@@ -160,14 +182,90 @@ def computeLoglikelihood_cnsPM(D, pars, phi=69.314):
     bg = {'n':np.median([len(x) for x in D]),'type':most_common}
     #Compute loglik across all chromosomes
     loglik=0
-    for d in D:
-        loglik+=computeLoglikelihood_cnsPM_single(d,pars,phi,bg)
+
+    print(err)
+    if err:
+        print("Running PAPI in error model mode")
+        for d in D:
+            loglik+=computeLoglikelihood_cnsPM_single_err(d,pars,phi,bg)
+    else:
+        print("Running PAPI in default (no error model) mode")
+        for d in D:
+            loglik+=computeLoglikelihood_cnsPM_single(d,pars)
 
 #}}}
     return(loglik)
 
 
-def computeLoglikelihood_cnsPM_single(D, pars, phi,bg):
+def computeLoglikelihood_cnsPM_single(D, pars):
+
+    '''
+    Takes in dict of parameter values ( e.g : [0.3,0.5,6,5]}) and the data D for a single chromosome ({'type':'10', 'length':1018} ... ). Additionally takes in an error function and parameters phi for the error function ( see err_func). Computes loglikelihood of data under our pooled markov model with censoring ( cns). 
+    '''
+#{{{ 
+
+    #Get params (lambdas)
+    params = computeParams_markovWF(pars,1e-6)
+    assert all([ x != 0 for x in params.values()]) 
+    #State space dict
+
+    StSp = {0:[0,0],1:[1,0],2:[0,1],3:[1,1]}
+
+    typDict = {'00':0,'10':1,'01':2,'11':3}
+    typDict_phs = {'00':[[0,0],[0,0]],'10':[[1,0],[0,1]],'11':[[1,1],[1,1]]} #Used only when there is one censored tract
+
+    
+    #params = {'lambdaA':[(1-p_A)*t_A, p_A*t_A], 'lambdaB':[(1-p_B)*t_B, p_B*t_B]} 
+    #Initialize P & Em matrices (2 * N )
+    #Compute entries of P matrix (2 * N ) 
+    if len(D) > 1:
+
+        P = np.empty((4,len(D)))
+
+        #Initialization
+        for k in [0,1,2,3]:
+            Em = {0:np.log([1, 0, 0 ,0]), 1:np.log([0,1,0,0]),2:np.log([0,1,0,0]),3:np.log([0,0,0,1])}  # Observed data is only '10'. 1 and 2 have identical emissions under this scheme
+            P[k,0] = np.log(params['lambdaA'][StSp[k][0]]) - params['lambdaA'][StSp[k][0]]*(D[0]['length']/100)  + np.log(params['lambdaB'][StSp[k][1]]) - params['lambdaB'][StSp[k][1]]*(D[0]['length']/100) + Em[k][typDict[D[0]['type']]]
+
+        N, N_cns, E = precompute_transitionMatrix(params,mode='cns')
+        
+        for col in range(1, len(D)): #Oth columns has already been computed 
+
+            Em = {0:np.log([1, 0, 0 ,0]), 1:np.log([0,1,0,0]),2:np.log([0,1,0,0]),3:np.log([0,0,0,1])}
+
+            if col == len(D)-1: #Censor last column
+                T = N_cns + E*(D[col]['length']/100)
+                #print('N_cns:\n {}'.format(N_cns))
+                #print('E:\n {}'.format(E))
+                #print('Transition Matrix :\n {}'.format(T))
+            else:
+                T = N + E*(D[col]['length']/100) #Divide by hundred to convert centimorgan to morgan
+
+                #print('N:\n {}'.format(N))
+                #print('E:\n {}'.format(E))
+                #print('Transition Matrix :\n {}'.format(T))
+            for k in [0,1,2,3]:
+                P[k,col]= logsumexp([\
+                        P[0,col-1] + T[0,k] + Em[k][typDict[D[col]['type']]],\
+                        P[1,col-1] + T[1,k] + Em[k][typDict[D[col]['type']]],\
+                        P[2,col-1] + T[2,k] + Em[k][typDict[D[col]['type']]],\
+                        P[3,col-1] + T[3,k] + Em[k][typDict[D[col]['type']]],\
+                        ])
+            
+    else: #Only one censored tract when len(D) == 1
+        P = np.empty((2,len(D)))
+        #print('One tract only , censoring')
+        for k in [0,1]:
+            phase = typDict_phs[D[0]['type']][k]
+            P[k,0] = -( params['lambdaA'][phase[0]]*(D[0]['length']/100) + params['lambdaB'][phase[1]]*(D[0]['length']/100) ) 
+   
+    
+#}}}
+    return(logsumexp(P[:,-1]))
+
+
+
+def computeLoglikelihood_cnsPM_single_err(D, pars, phi,bg):
 
     '''
     Takes in dict of parameter values ( e.g : [0.3,0.5,6,5]}) and the data D for a single chromosome ({'type':'10', 'length':1018} ... ). Additionally takes in an error function and parameters phi for the error function ( see err_func). Computes loglikelihood of data under our pooled markov model with censoring ( cns). 
@@ -209,11 +307,13 @@ def computeLoglikelihood_cnsPM_single(D, pars, phi,bg):
         for col in range(1, len(D)): #Oth columns has already been computed 
             loge = err_func(D[col], phi,bg)
 
+
             Em = {0:[np.log(1-np.exp(loge)), loge - np.log(2), -np.inf ,loge-np.log(2)],\
                     1:[loge-np.log(2),np.log(1-np.exp(loge)),-np.inf,loge-np.log(2)],\
                     2:[loge-np.log(2),np.log(1-np.exp(loge)),-np.inf,loge-np.log(2)],\
                     3:[loge-np.log(2),loge-np.log(2),-np.inf,np.log(1-np.exp(loge))]\
                     }  # Observed data is only '10'. 1 and 2 have identical emissions under this scheme
+
             if col == len(D)-1: #Censor last column
                 T = N_cns + E*(D[col]['length']/100)
                 #print('N_cns:\n {}'.format(N_cns))
@@ -245,72 +345,72 @@ def computeLoglikelihood_cnsPM_single(D, pars, phi,bg):
     return(logsumexp(P[:,-1]))
 
 
-def computeLoglikelihood_cnsPM_flaterr(D, pars,e):
-    '''
-    Takes in dict of parameter values ( e.g : [0.3,0.5,6,5]}) and the data D for a single chromosome ({'type':'10', 'length':1018} ... ). Computes loglikelihood of data under our pooled markov model with censoring ( cns)
-    '''
-#{{{ 
-
-    #Get params (lambdas)
-    params = computeParams_markovWF(pars,1e-6)
-    print(params)
-    assert all([ x != 0 for x in params.values()]) 
-    #State space dict
-
-    StSp = {0:[0,0],1:[1,0],2:[0,1],3:[1,1]}
-
-    typDict = {'00':0,'10':1,'01':2,'11':3}
-    typDict_phs = {'00':[[0,0],[0,0]],'10':[[1,0],[0,1]],'11':[[1,1],[1,1]]}
-
-    Em = {0:np.log([1-e, e/2, 0 ,e/2]), 1:np.log([e/2,1-e,0,e/2]),2:np.log([e/2,1-e,0,e/2]),3:np.log([e/2,e/2,0,1-e])}  # Observed data is only '10'. 1 and 2 have identical emissions under this scheme
-
-    #params = {'lambdaA':[(1-p_A)*t_A, p_A*t_A], 'lambdaB':[(1-p_B)*t_B, p_B*t_B]} 
-    #Initialize P & Em matrices (2 * N )
-    #Compute entries of P matrix (2 * N ) 
-    if len(D) > 1:
-
-        P = np.empty((4,len(D)))
-
-        #Initialization
-        for k in [0,1,2,3]:
-
-            P[k,0] = np.log(params['lambdaA'][StSp[k][0]]) - params['lambdaA'][StSp[k][0]]*(D[0]['length']/100)  + np.log(params['lambdaB'][StSp[k][1]]) - params['lambdaB'][StSp[k][1]]*(D[0]['length']/100) + Em[k][typDict[D[0]['type']]]
-
-        N, N_cns, E = precompute_transitionMatrix(params,mode='cns')
-
-        for col in range(1, len(D)): #Oth columns has already been computed 
-
-            if col == len(D)-1: #Censor last column
-                print('censoring')
-                T = N_cns + E*(D[col]['length']/100)
-                #print('N_cns:\n {}'.format(N_cns))
-                #print('E:\n {}'.format(E))
-                #print('Transition Matrix :\n {}'.format(T))
-            else:
-                T = N + E*(D[col]['length']/100) #Divide by hundred to convert centimorgan to morgan
-
-                #print('N:\n {}'.format(N))
-                #print('E:\n {}'.format(E))
-                #print('Transition Matrix :\n {}'.format(T))
-            for k in [0,1,2,3]:
-                P[k,col]= logsumexp([\
-                        P[0,col-1] + T[0,k] + Em[k][typDict[D[col]['type']]],\
-                        P[1,col-1] + T[1,k] + Em[k][typDict[D[col]['type']]],\
-                        P[2,col-1] + T[2,k] + Em[k][typDict[D[col]['type']]],\
-                        P[3,col-1] + T[3,k] + Em[k][typDict[D[col]['type']]],\
-                        ])
-
-    else: #Only one censored tract when len(D) == 1
-        P = np.empty((2,len(D)))
-        print('One tract only , censoring')
-        for k in [0,1]:
-            phase = typDict_phs[D[0]['type']][k]
-            P[k,0] = -( params['lambdaA'][phase[0]]*(D[0]['length']/100) + params['lambdaB'][phase[1]]*(D[0]['length']/100) ) 
-    
-
-    #print('Final P:{}'.format(P))
-#}}}
-    return(logsumexp(P[:,-1]))
+#def computeLoglikelihood_cnsPM_flaterr(D, pars,e):
+#    '''
+#    Takes in dict of parameter values ( e.g : [0.3,0.5,6,5]}) and the data D for a single chromosome ({'type':'10', 'length':1018} ... ). Computes loglikelihood of data under our pooled markov model with censoring ( cns)
+#    '''
+##{{{ 
+#
+#    #Get params (lambdas)
+#    params = computeParams_markovWF(pars,1e-6)
+#    print(params)
+#    assert all([ x != 0 for x in params.values()]) 
+#    #State space dict
+#
+#    StSp = {0:[0,0],1:[1,0],2:[0,1],3:[1,1]}
+#
+#    typDict = {'00':0,'10':1,'01':2,'11':3}
+#    typDict_phs = {'00':[[0,0],[0,0]],'10':[[1,0],[0,1]],'11':[[1,1],[1,1]]}
+#
+#    Em = {0:np.log([1-e, e/2, 0 ,e/2]), 1:np.log([e/2,1-e,0,e/2]),2:np.log([e/2,1-e,0,e/2]),3:np.log([e/2,e/2,0,1-e])}  # Observed data is only '10'. 1 and 2 have identical emissions under this scheme
+#
+#    #params = {'lambdaA':[(1-p_A)*t_A, p_A*t_A], 'lambdaB':[(1-p_B)*t_B, p_B*t_B]} 
+#    #Initialize P & Em matrices (2 * N )
+#    #Compute entries of P matrix (2 * N ) 
+#    if len(D) > 1:
+#
+#        P = np.empty((4,len(D)))
+#
+#        #Initialization
+#        for k in [0,1,2,3]:
+#
+#            P[k,0] = np.log(params['lambdaA'][StSp[k][0]]) - params['lambdaA'][StSp[k][0]]*(D[0]['length']/100)  + np.log(params['lambdaB'][StSp[k][1]]) - params['lambdaB'][StSp[k][1]]*(D[0]['length']/100) + Em[k][typDict[D[0]['type']]]
+#
+#        N, N_cns, E = precompute_transitionMatrix(params,mode='cns')
+#
+#        for col in range(1, len(D)): #Oth columns has already been computed 
+#
+#            if col == len(D)-1: #Censor last column
+#                print('censoring')
+#                T = N_cns + E*(D[col]['length']/100)
+#                #print('N_cns:\n {}'.format(N_cns))
+#                #print('E:\n {}'.format(E))
+#                #print('Transition Matrix :\n {}'.format(T))
+#            else:
+#                T = N + E*(D[col]['length']/100) #Divide by hundred to convert centimorgan to morgan
+#
+#                #print('N:\n {}'.format(N))
+#                #print('E:\n {}'.format(E))
+#                #print('Transition Matrix :\n {}'.format(T))
+#            for k in [0,1,2,3]:
+#                P[k,col]= logsumexp([\
+#                        P[0,col-1] + T[0,k] + Em[k][typDict[D[col]['type']]],\
+#                        P[1,col-1] + T[1,k] + Em[k][typDict[D[col]['type']]],\
+#                        P[2,col-1] + T[2,k] + Em[k][typDict[D[col]['type']]],\
+#                        P[3,col-1] + T[3,k] + Em[k][typDict[D[col]['type']]],\
+#                        ])
+#
+#    else: #Only one censored tract when len(D) == 1
+#        P = np.empty((2,len(D)))
+#        print('One tract only , censoring')
+#        for k in [0,1]:
+#            phase = typDict_phs[D[0]['type']][k]
+#            P[k,0] = -( params['lambdaA'][phase[0]]*(D[0]['length']/100) + params['lambdaB'][phase[1]]*(D[0]['length']/100) ) 
+#    
+#
+#    #print('Final P:{}'.format(P))
+##}}}
+#    return(logsumexp(P[:,-1]))
 
 
 
